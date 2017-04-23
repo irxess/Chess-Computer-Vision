ae115c6db1aea06827744b7f71e86a155b7e6e9b
Harald Husum <harahu@stud.ntnu.no>
2017-04-23 14:13:18 +0200

    stuff

--- chess_deep.py
+++ chess_deep.py
@@ -23,15 +23,15 @@ def deepnn(x):
     """
 
     # First convolutional layer - maps one rgb image to 32 feature maps.
-    W_conv1 = weight_variable([5, 5, 3, 32])
-    b_conv1 = bias_variable([32])
+    W_conv1 = weight_variable([5, 5, 3, 64])
+    b_conv1 = bias_variable([64])
     h_conv1 = tf.nn.elu(conv2d(x, W_conv1) + b_conv1)
 
     # Pooling layer - downsamples by 2X.
     h_pool1 = max_pool_2x2(h_conv1)
 
     # Second convolutional layer -- maps 32 feature maps to 64.
-    W_conv2 = weight_variable([5, 5, 32, 64])
+    W_conv2 = weight_variable([5, 5, 64, 64])
     b_conv2 = bias_variable([64])
     h_conv2 = tf.nn.elu(conv2d(h_pool1, W_conv2) + b_conv2)
 
